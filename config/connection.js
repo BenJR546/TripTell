a3efee11ec68e5a2a96e7968d227270dc0c899e6
BenJR546 <162855613+BenJR546@users.noreply.github.com>
2024-09-05 19:29:36 +1000

    map functionality + db

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    }
-  );
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+      }
+    );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const path = require('path');  
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const path = require("path");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   },
@@ -25,23 +25,30 @@ const sess = {
 
 app.use(session(sess));
 
-
 // Set the views directory
-app.set('views', path.join(__dirname, 'views')); 
-
+app.set("views", path.join(__dirname, "views"));
 
 const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    partialsDir: "views",
+  })
+);
+app.get("/leaflettest", (req, res) => {
+  res.render("leaflettest");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`Now listening on http://localhost:${PORT}`)
+  );
 });
